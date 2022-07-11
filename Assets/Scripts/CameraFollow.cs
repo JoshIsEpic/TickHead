406d26d638e3593ff7e16705e32bf45db97ecabf
JoshIsEpic <josh.a.jones.cybersecure@gmail.com>
2022-07-11 01:27:43 -0500

    TickHead Release 1.0

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("Target Object")]
+    public Transform target;
+
+    private void Update()
+    {
+        if(target.position.y > transform.position.y)
+        {
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        }
+    }
+}
